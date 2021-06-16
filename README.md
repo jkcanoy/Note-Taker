5383550bffb424b72d3f784d4a61845cc3838b4f
Kyle Canoy <kcanoy@uw.edu>
2021-06-16 00:22:21 -0700

    write delete request

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Note-Taker
-11 homework
+
+  ![License](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+  ## Description
+  This application is a note taker that uses an express server. The user is able to create, retrieve, and delete notes.
+
+  ## Table of Contents
+  - [Description](#description)
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+  1. Clone Note-Taker repo from GitHub.
+  2. Access repo using the command line.
+  3. In the terminal run ```npm i```
+  
+  ## Usage
+  1. Access Note-Taker through the command line on your local machine. 
+  2. Run ```npm run watch``` 
+  3. Open browser and go to http://localhost:3000/. 
+  4. Fill out the text input and click the save icon on the top right. Saved notes will appear on the left.
+  5. Click the + icon to create a new note.
+  6. Click the red garbage bin icon to delete any saved notes.
+
+  ## License
+  This application is covered by MIT License. For more information please refer to https://choosealicense.com/licenses/mit/
+
+  ## Questions
+  * GitHub: [jkcanoy](https://github.com/jkcanoy). 
+
+  * For more information please contact kylecanoy96@gmail.com.
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,34 +11,37 @@ module.exports = (app) => {
 
     // API POST request
     app.post('/api/notes', (req, res) => {
+        let note = req.body
         // assign unique id using uuid npm package
         req.body.id = uuidv4();
 
-        console.log(`New Note: ${JSON.stringify(req.body)}`)
+        console.log(`New Note: ${JSON.stringify(note)}`)
 
         // push new written note to json array
-        database.push(req.body)
+        database.push(note)
 
         // Write new note to db.json
-        fs.writeFileSync('./db/db.json', JSON.stringify(req.body))
+        fs.writeFileSync('./db/db.json', JSON.stringify(database))
 
         console.log("Successfully added new note to db.json")
 
-        res.json(req.body)
+        res.json(note)
     });
 
-    // app.delete('/api/notes/:id', (req, res) => {
+    app.delete('/api/notes/:id', (req, res) => {
 
-    //     let noteId = req.params.id.toString();
-    //     console.log(noteId)
+        let noteId = req.params.id.toString();
+        console.log(noteId)
 
-    //     for(i = 0; i < database.length; i++) {
-    //         if(database[i].id == id) {
-    //             res.send(database[i]);
+        for(i = 0; i < database.length; i++) {
+            if(database[i].id == noteId) {
+                res.send(database[i]);
 
-    //             database.splice(i,1);
-    //             break;
-    //         }
-    //     }
-    // })
+                database.splice(i,1);
+                break;
+            }
+        }
+
+        fs.writeFileSync('./db/db.json', JSON.stringify(database));
+    });
 }
